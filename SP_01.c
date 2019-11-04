dac5186f912f18e9141d0007c6ac035757c239cd
filadendronfesb <56310150+filadendronfesb@users.noreply.github.com>
2019-11-04 21:47:12 +0100

    Update SP_01.c

--- SP_01.c
+++ SP_01.c
@@ -3,30 +3,24 @@
 #include <malloc.h>
 #include <string.h>
 
-
 typedef struct {
-	char* ime;
-	char* prez;
+	char *ime;
+	char *prez;
 	int bod;
 }_stud;
 
 int BrojimRedtke();
-void UzimamPodatke(int, _stud*);
-void Ispisujem(int, _stud*);
+int UzimamPodatke(int, _stud *);
+int Ispisujem(int, _stud *);
 
 int main()
 {
-	int br;
-
-
-	_stud* student;
-
+	int br = 0;
+	_stud *student = NULL;
 
 	br = BrojimRedtke();
 
-	student = (_stud*)malloc(br * sizeof(_stud));
-		if (student == NULL);
-			printf("Neuspijela alokacija memorije\n");
+	student = (_stud *)malloc(br * sizeof(_stud));
 
 	UzimamPodatke(br, student);
 
@@ -34,97 +28,76 @@ int main()
 
 	return 0;
 }
-
-
 int BrojimRedtke()
 {
-	char c;
-	int br;
-	FILE* f;
+	char c = 0;
+	int br = 0;
+	FILE *f = NULL;
 
 	f = fopen("studenti.txt", "r");
 
-
 	if (f == NULL)
-	{
 		printf("Error!");
-	}
-
-	br = 1;//u zadnjem red nema newline-a
 
 	do {
-
 		c = getc(f);
+
 		if (c == '\n')
 			br++;
 
 	} while (feof(f) == 0);
 
-
 	fclose(f);
 
 	return br;
-
 }
-void UzimamPodatke(int br, _stud* student)
+int UzimamPodatke(int br, _stud *student)
 {
-	int i;
-	char temp[30];
-	char temp2[30];
-	FILE* f;
+	int i = 0;
+	char temp[30] = {0};
+	char temp2[30] = {0};
+	FILE *f = NULL;
 
 	f = fopen("studenti.txt", "r");
 
-
 	if (f == NULL)
-	{
 		printf("Error!");
-	}
-
-
 
 	for (i = 0; i < br; i++)
 	{
-
 		fscanf(f, "%d %s %s", &student[i].bod, temp, temp2);
 
-		student[i].ime = (char*)malloc(strlen(temp) * sizeof(char));
-		if (student[i].ime == NULL);
-			printf("Neuspijela alokacija memorije\n");
-		student[i].prez = (char*)malloc(strlen(temp2) * sizeof(char));
-		if (student[i].prez == NULL);
-			printf("Neuspijela alokacija memorije\n");
+		student[i].ime = (char *)malloc(strlen(temp) * sizeof(char));
+		if (student[i].ime == NULL)
+			printf("Error!");
+		student[i].prez = (char *)malloc(strlen(temp2) * sizeof(char));
+		if (student[i].prez == NULL)
+			printf("Erorr!");
 
 		strcpy(student[i].ime, temp);
 		strcpy(student[i].prez, temp2);
-
 	}
 
 	fclose(f);
 
+	return 0;
 }
-void Ispisujem(int br, _stud* student)
+int Ispisujem(int br, _stud *student)
 {
-	int i;
-	FILE* f;
+	int i = 0;
+	FILE *f = NULL;
 
 	f = fopen("studenti.txt", "r");
 
-
 	if (f == NULL)
-	{
 		printf("Error!");
-	}
-
-
 
 	printf("\tIme    Prezime\t RE Bod\t\t\t APS Bod\n");
 
 	for (i = 0; i < br; i++)
-	{
 		printf(" %10s %10s \t %.2f percent \t\t %d \n", student[i].ime, student[i].prez, (float)((student[i].bod / (float)50) * 100), student[i].bod);
-	}
-
 
 	fclose(f);
+
+	return 0;
 }
