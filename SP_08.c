816f7e6311c74bf5efd50186f53d72aa6f4d23f4
filadendronfesb <56310150+filadendronfesb@users.noreply.github.com>
2020-01-07 12:38:05 +0100

    Add files via upload
    prepravljeni

--- SP_08.c
+++ SP_08.c
@@ -1,353 +1,235 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<ctype.h>
-#define G printf("GRESKA!!!")
-#define MAX 1024
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
 
-struct Folder;
-typedef struct Folder* position;
-typedef struct Folder {
-	char *name;
-	position sibling;
-	position child;
-}FLD;
+struct Direktorij;
+typedef struct Direktorij *pozicija;
+typedef struct Direktorij {
+	char *dirname;
+	pozicija sibling;
+	pozicija child;
+}DIR;
 
-struct stog;
-typedef struct stog * pos;
-typedef struct stog {
-	position folder;
-	pos next;
+struct Stog;
+typedef struct Stog *poz;
+typedef struct Stog {
+	pozicija dir;
+	poz next;
 }ST;
 
-int CreateRoot(position *);
-int CreateHead(pos *);
-int Allocation_of_Buffers(char **, char **, char **, char **, char **);
-int Print(char *);
-int check(char *);
-int Make_Directory(position, char *);
-position Insert_Directory(position, position);
-int Directory(position, char *);
-position Enter_Directory(position, char *, pos, char *);
-int PushStog(pos, position);
-position Exit_Directory(position, char *, pos);
-int PopStog(pos stog, position *temp);
+int izbor(char *);
+pozicija Dodavanje_Direktorija(pozicija, char *);
+int ispis_poddirektorija(pozicija);
+pozicija Poddirektorij(pozicija, char *, poz);
+int Stavljanje_na_stog(pozicija, poz);
+pozicija Naddirektorij(pozicija, poz);
 
 int main()
 {
-	position root = NULL;
-	position last = NULL;
-	pos stog = NULL;
-	char *dirname = NULL;
-	char *instruction = NULL;
-	char *path = NULL;
+	DIR root;
+	root.sibling = NULL;
+	root.child = NULL;
+	char *komandna_linija = NULL;
 	char *buff = NULL;
-	char *command = NULL;
-	int count = 0;
+	int pos = 0;
+	pozicija last = NULL;
+	ST stog;
+	stog.dir = NULL;
+	stog.next = NULL;
+	poz NewNode = NULL;
 
 
-	if (Allocation_of_Buffers(&path, &dirname, &instruction, &buff, &command) != 0)
-		G;
+	root.dirname = (char *)malloc(4 * sizeof(char));
+	if (root.dirname == NULL)
+		printf("Greska!!!\n");
 
-	if (CreateRoot(&root) != 0)
-		G;
+	strcpy(root.dirname, "Root");
 
-	last = root;
+	last = &root;
 
-	if (CreateHead(&stog) != 0)
-		G;
+	komandna_linija = (char *)malloc(30 * sizeof(char));
+	if (komandna_linija == NULL)
+		printf("Greska!!!\n");
 
-	while (strcmp(instruction, "exit") != 0)
+	memset(komandna_linija, 0, 30);
+
+	buff = (char *)malloc(30 * sizeof(char));
+	if (buff == NULL)
+		printf("Greska!!!\n");
+
+	memset(buff, 0, 30);
+
+	NewNode = (poz)malloc(sizeof(ST));
+	if (NewNode == NULL)
+		printf("Greska!!!\n");
+
+	NewNode->next = NULL;
+	NewNode->dir = NULL;
+
+	NewNode->dir = last;
+	NewNode->next = stog.next;
+	stog.next = NewNode;
+
+	while (strcmp(buff, "exit"))
 	{
-		if (Print(path) != 0)
-			G;
+		memset(komandna_linija, 0, 30);
+		gets(komandna_linija);
+		sscanf(komandna_linija, "%s%n", buff, &pos);
 
-		memset(command, 0, MAX);
-		memset(dirname, 0, MAX);
-		memset(instruction, 0, MAX);
-
-		fgets(command, MAX, stdin);
-		count = sscanf(command, " %s %s", instruction, dirname);
-
-		switch (check(instruction))
+		switch (izbor(buff))
 		{
-		case 1:
-			if (count != 2)
-				printf("The syntax of the command is incorrect.\n");
-			else
-			{
-				if (Make_Directory(last, dirname) != 0)
-					G;
-			}
+		case 1://md
+			memset(buff, 0, 30);
+			sscanf(komandna_linija + pos, "%s", buff);
+			last = Dodavanje_Direktorija(last, buff);
 			break;
-		case 2:
-			if (count != 1)
-				printf("The syntax of the command is incorrect.\n");
-			else
-				last = Exit_Directory(last, path, stog);
+		case 2://cd..
+			last = Naddirektorij(last, &stog);
 			break;
-		case 3:
-			if(count != 2)
-				printf("The syntax of the command is incorrect.\n");
-			else
-				last = Enter_Directory(last->child, dirname, stog, path);
+		case 3://cd dir
+			memset(buff, 0, 30);
+			sscanf(komandna_linija + pos, "%s", buff);
+			last = Poddirektorij(last->child, buff, &stog);
 			break;
-		case 4:
-			if (count != 1)
-				printf("The syntax of the command is incorrect.\n");
-			else
-			{
-				if (Directory(last->child, path) != 0)
-					G;
-			}
+		case 4://dir
+			if (ispis_poddirektorija(last) != 0)
+				printf("Greska!!!\n");
 			break;
-		case 5:
-			if (count != 1)
-				printf("The syntax of the command is incorrect.\n");
+		case 5://exit
 			break;
 		default:
-			printf("'%s' is not recognized as an internal or external command, operable program or batch file.\n", instruction);
+			printf("%s is not recognized as an internal or external command, operable program or batch file.", buff);
 			break;
 		}
 	}
 
+	free(komandna_linija);
+	free(buff);
+	
 	return 0;
 }
-position Exit_Directory(position last, char *path, pos stog)
+pozicija Naddirektorij(pozicija last, poz stog)
 {
-	position temp = NULL;
+	poz tmp = NULL;
+	pozicija temp = NULL;
 
-	if (PopStog(stog, &temp) != 0)
-		G;
+	tmp = stog->next;
+	stog->next = tmp->next;
+
+	temp = stog->next->dir;
+	free(tmp);
 
 	return temp;
 }
-int PopStog(pos stog, position *temp)
+pozicija Poddirektorij(pozicija lastchild, char *buff, poz stog)
 {
-	pos temp1 = NULL;
+	pozicija trenutni = NULL;
 
-	temp1 = stog->next;
-	stog->next = temp1->next;
+	trenutni = lastchild;
 
-	*temp = temp1->next;
+	while (strcmp(trenutni->dirname, buff) && trenutni != NULL)
+		trenutni = trenutni->sibling;
 
-	free(temp1);
 
-	return 0;
+	if (Stavljanje_na_stog(trenutni, stog) != 0)
+		printf("Greska!!!\n");
+
+	return trenutni;
 }
-position Enter_Directory(position last, char *dirname, pos stog, char *path)
+int Stavljanje_na_stog(pozicija trenutni, poz stog)
 {
-	position curr = NULL;
-	curr = last;
-	while (curr != NULL && (strcmp(curr->name, dirname) != 0))
-		curr = curr->sibling;
+	poz NewNode = NULL;
 
-	if (curr == NULL)
-	{
-		printf("The system cannit find the path specified.");
-		return last;
-	}
+	NewNode = (poz)malloc(sizeof(ST));
+	if (NewNode == NULL)
+		printf("Greska!!!\n");
 
-	if (PushStog(stog, last) != 0)
-		G;
+	NewNode->next = NULL;
+	NewNode->dir = NULL;
 
-	strcat(path, "\\");
-	strcat(path, curr->name);
 
-	return curr;
-}
-int PushStog(pos stog, position last)
-{
-	pos NewNode = NULL;
-
-	if (CreateHead(&NewNode) != 0)
-		G;
-
-	NewNode->folder = last;
+	NewNode->dir = trenutni;
 	NewNode->next = stog->next;
 	stog->next = NewNode;
 
 	return 0;
 }
-int Directory(position last, char *path)
+int ispis_poddirektorija(pozicija last)
 {
-	position cur = NULL;
-	
-	if (*path == 0)
-		printf("\nRoot : \n\n");
-	else
-		printf("\n Directory of %s \n", path);
-	
-	while (last != NULL)
+	pozicija trenutni = NULL;
+
+	trenutni = last->child;
+
+	printf("\nDirectory of %s\n\n", last->dirname);
+
+	while (trenutni != NULL)
 	{
-		printf("%s\n", last->name);
-		last = last->sibling;
-	} 
+		printf("<DIR>\t%s\n", trenutni->dirname);
+		trenutni = trenutni->sibling;
+	}
 
 	return 0;
 }
-position Insert_Directory(position NN, position child)
+pozicija Dodavanje_Direktorija(pozicija last, char *dirname)
 {
-	position prev = NULL;
-	prev = child;
+	pozicija Novi_cvor = NULL;
+	pozicija prethodni = NULL;
 
-	if (prev == NULL)
-		return NN;
+	Novi_cvor = (pozicija)malloc(sizeof(DIR));
+	if (Novi_cvor == NULL)
+		printf("Greska!!!\n");
 
-	if (strcmp(NN->name, prev->name) < 0)
+	Novi_cvor->dirname = (char *)malloc(30 * sizeof(char));
+	if (Novi_cvor->dirname == NULL)
+		printf("Greska!!!\n");
+
+	strcpy(Novi_cvor->dirname, dirname);
+	Novi_cvor->child = NULL;
+	Novi_cvor->sibling = NULL;
+
+	if (last->child == NULL)
 	{
-		NN->sibling = prev;
-		return NN;
+		Novi_cvor->child = last->child;
+		last->child = Novi_cvor;
+
+		return last;
 	}
-	else if (strcmp(NN->name, prev->name) == 0)
+
+	prethodni = last->child;
+
+	while (prethodni->sibling != NULL && (strcmp(prethodni->sibling->dirname, Novi_cvor->dirname) < 0))
+		prethodni = prethodni->sibling;
+
+	if (prethodni != NULL && (strcmp(prethodni->dirname, Novi_cvor->dirname) < 0))
 	{
-		printf("Folder already exists!!!");
-		return child;
+		Novi_cvor->sibling = prethodni->sibling;
+		prethodni->sibling = Novi_cvor;
+
+		return last;
 	}
 	else
 	{
-		while ((prev->sibling != NULL) && (strcmp(NN->name, prev->sibling->name) > 0))
-			prev = prev->sibling;
-		if ((prev->sibling != NULL) && (strcmp(NN->name, prev->sibling->name) == 0))
-		{
-			printf("Folder already exists!!!");
-			return child;
-		}
+		Novi_cvor->sibling = last->child;
+		last->child = Novi_cvor;
+		
+		return last;
 	}
 
-	NN->sibling = prev->sibling;
-	prev->sibling = NN;
 
-	return child;
+	
 }
-int Make_Directory(position p, char *dirname)
+int izbor(char *komanda)
 {
-	position NewNode = NULL;
-
-	NewNode = (position)malloc(sizeof(FLD));
-	if (NewNode == NULL)
-		G;
-
-	NewNode->child = NULL;
-	NewNode->sibling = NULL;
-
-	if (strlen(dirname) != 0) {
-		NewNode->name = (char *)malloc(strlen(dirname)* sizeof(char));
-		if (NewNode->name == NULL)
-			G;
-	}
-
-	strcpy(NewNode->name, dirname);
-
-	p->child = Insert_Directory(NewNode, p->child);
-
-	return 0;
-}
-int check(char *instr)
-{
-	if (!strcmp(instr, "md"))
+	if (!strcmp(komanda, "md"))
 		return 1;
-	else if (!strcmp(instr, "cd.."))
+	else if (!strcmp(komanda, "cd.."))
 		return 2;
-	else if (!strcmp(instr, "cd"))
+	else if (!strcmp(komanda, "cd"))
 		return 3;
-	else if (!strcmp(instr, "dir"))
+	else if (!strcmp(komanda, "dir"))
 		return 4;
-	else if (!strcmp(instr, "exit"))
+	else if (!strcmp(komanda, "exit"));
 		return 5;
-	else
-		return 0;
-}
-int Print(char *path)
-{
-	printf("%s>", path);
-
-	return 0;
-}
-int Allocation_of_Buffers(char **ppath, char **pdirname, char **pinstruction, char **pbuff, char **pcommand)
-{
-	char *path = NULL;
-	char *dirname = NULL;
-	char *instruction = NULL;
-	char *buff = NULL;
-	char *command = NULL;
-
-	path = (char *)malloc(MAX * sizeof(char));
-	if (path == NULL)
-		G;
-
-	memset(path, '\0', MAX);
-
-	*ppath = path;
-
-	dirname = (char *)malloc(MAX * sizeof(char));
-	if (dirname == NULL)
-		G;
-
-	memset(dirname, '\0', MAX);
-
-	*pdirname = dirname;
-
-	instruction = (char *)malloc(MAX * sizeof(char));
-	if (instruction == NULL)
-		G;
-
-	memset(instruction, '\0', MAX);
-
-	*pinstruction = instruction;
-
-	buff = (char *)malloc(MAX * sizeof(char));
-	if (buff == NULL)
-		G;
-
-	memset(buff, '\0', MAX);
-
-	*pbuff = buff;
-
-	command = (char *)malloc(MAX * sizeof(char));
-	if (command == NULL)
-		G;
-
-	memset(command, '\0', MAX);
-
-	*pcommand = command;
-
-	return 0;
-}
-int CreateHead(pos *p)
-{
-	pos NewNode = NULL;
-
-	NewNode = (pos)malloc(sizeof(ST));
-	if (NewNode == NULL)
-		G;
-
-	NewNode->folder = NULL;
-	NewNode->next = NULL;
-
-	*p = NewNode;
-
-	return 0;
-}
-int CreateRoot(position *p)
-{
-	position NewNode = NULL;
-
-	NewNode = (position)malloc(sizeof(FLD));
-	if (NewNode == NULL)
-		G;
-
-	NewNode->child = NULL;
-	NewNode->sibling = NULL;
-
-	NewNode->name = (char *)malloc(4 * sizeof(char));
-	if (NewNode->name == NULL)
-		G;
-
-	NewNode->name = "Root";
-
-	*p = NewNode;
-
-	return 0;
 }
